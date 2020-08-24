3b0e40e6049375d637bb1dbed421054b8ade49ee
bartwr <mail@bartroorda.nl>
2020-08-24 23:52:52 +0200

    Added Landing intro: Wij Nieuwe Westen

--- imports/ui/components/Landing/Landing.jsx
+++ imports/ui/components/Landing/Landing.jsx
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
+import 'animate.css';
+
+import './Landing.css'
 
 class App extends Component {
   constructor(props) {
     super(props)
   }
   componentDidMount() {
-
+    setTimeout(() => {
+      FlowRouter.go('/kaart')
+    }, 3000)
   }
   render() {
-    return <div className="Landing">
-      Landing
-    </div>
+    return <a href="/kaart" className="Landing px-6 block">
+      <div className="animate__animated animate__pulse">Wij</div>
+      <div className="text-center animate__animated animate__pulse animate__delay-1s" style={{marginLeft: '-10vw'}}>Nieuwe</div>
+      <div className="text-right animate__animated animate__pulse animate__delay-2s">Westen</div>
+    </a>
   }
 }
 
--- imports/ui/components/Map/Map.jsx
+++ imports/ui/components/Map/Map.jsx
@@ -21,7 +21,7 @@ class Map extends Component {
     return <LeafletMap center={position} zoom={15}>
       <TileLayer
         minZoom="14"
-        maxZoom="22"
+        maxZoom="19"
         url={`https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}`}
         attribution={`Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community`}
       />
