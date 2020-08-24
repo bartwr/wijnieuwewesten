ff2bb8ad8f1ca0ad189ae78fc0c369e190500b7b
bartwr <mail@bartroorda.nl>
2020-08-24 23:57:03 +0200

    Make site available on mobile

--- imports/ui/components/Landing/Landing.jsx
+++ imports/ui/components/Landing/Landing.jsx
@@ -15,8 +15,8 @@ class App extends Component {
   render() {
     return <a href="/kaart" className="Landing px-6 block">
       <div className="animate__animated animate__pulse">Wij</div>
-      <div className="text-center animate__animated animate__pulse animate__delay-1s" style={{marginLeft: '-10vw'}}>Nieuwe</div>
-      <div className="text-right animate__animated animate__pulse animate__delay-2s">Westen</div>
+      <div className="md:text-center animate__animated animate__pulse animate__delay-1s">Nieuwe</div>
+      <div className="md:text-right animate__animated animate__pulse animate__delay-2s">Westen</div>
     </a>
   }
 }
